bb3a0b6e327667da494ceeda1d4e4607c156ab93
overdiv <stancode203@gmail.com>
2019-02-24 20:02:06 +0900

    create dir : servcer_side_javascript

---
+++ server_side_javascript/webserver.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const dt = require('./datamodule');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.write(dt.myDateTime() + '\n');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
